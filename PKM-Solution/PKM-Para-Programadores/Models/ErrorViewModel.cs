748f709d731601cec9c3f1303432591ffaf9a623
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2026-02-12 22:32:11 -0300

    Initial project structure with renamed solution and project setup

---
+++ PKM-Solution/PKM-Para-Programadores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PKM_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
